d27ce6e2340c14236e1bd2a03ed476de6bfbb492
Viktor <vv.zeipel@gmail.com>
2019-12-25 17:06:18 +0100

    Added root client

---
+++ client/apollo.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  client: {
+    name: "Week reservatation [web]",
+    service: "week-reservation"
+  }
+};
---
+++ client/src/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { resolvers, typeDefs } from "./resolvers";
+import injectStyles from "./styles";
+
+// Set up our apollo-client to point at the server we created
+// this can be local or a remote endpoint
+const cache = new InMemoryCache();
+const client = new ApolloClient({
+  cache,
+  link: new HttpLink({
+    uri: "http://localhost:4000/graphql",
+    headers: {
+      authorization: localStorage.getItem("token"),
+      "client-name": "Week reservatation [web]",
+      "client-version": "1.0.0"
+    }
+  }),
+  resolvers,
+  typeDefs
+});
+
+cache.writeData({
+  data: {
+    isLoggedIn: !!localStorage.getItem("token"),
+    cartItems: []
+  }
+});
+
+injectStyles();
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <p>{"Hello from Apollo"}</p>
+  </ApolloProvider>,
+  document.getElementById("root")
+);
