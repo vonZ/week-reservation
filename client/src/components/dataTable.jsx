50c3ede8f15eb14c20437c27ae6f0d5d5fce54e0
Viktor <vv.zeipel@gmail.com>
2019-12-26 06:48:01 +0100

    Added DataTable component

--- client/src/components/dataTable.jsx
+++ client/src/components/dataTable.jsx
@@ -1,16 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Container from 'react-bootstrap/Container';
+import Table from 'react-bootstrap/Table';
 
-const DataTable = props => {
-    return (
-        <div>
-            
-        </div>
-    );
+const DataTable = (props) => {
+	const { tableData = [] } = props;
+
+	const renderRowCell = (cellData) => {
+		const data = Object.values(cellData);
+		const itemToReturn = data.map((item) => <td>{item}</td>);
+		return itemToReturn;
+	};
+
+	const renderRowHeading = (cellData) => {
+		const data = Object.keys(cellData);
+		const itemToReturn = data.map((item) => <th>{item}</th>);
+		return itemToReturn;
+	};
+
+	return (
+		<Container>
+			<Table responsive hover>
+				<thead>
+					<tr>{renderRowHeading(tableData[0])}</tr>
+				</thead>
+				<tbody>
+					{tableData.map((data, index) => (
+						<tr>{renderRowCell(data)}</tr>
+					))}
+				</tbody>
+			</Table>
+		</Container>
+	);
 };
 
 DataTable.propTypes = {
-    
+  tableData: PropTypes.array
 };
 
 export default DataTable;
--- client/src/pages/reservations.jsx
+++ client/src/pages/reservations.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
-import Container from 'react-bootstrap/Container';
-import Table from 'react-bootstrap/Table';
+import DataTable from '../components/dataTable';
 
 export const GET_ALL_RESERVATIONS = gql`
 	query getAllReservations {
@@ -22,33 +21,10 @@ export const GET_ALL_RESERVATIONS = gql`
 const Reservations = () => {
 	const { data, loading, error } = useQuery(GET_ALL_RESERVATIONS);
 	if (loading) return <p>LOADING</p>;
-	if (error) return <p>ERROR</p>;
-
-	const renderRowCell = (cellData) => {
-		const data = Object.values(cellData);
-		const itemToReturn = data.map((item) => <td>{item}</td>);
-		return itemToReturn;
-	};
-
-	const renderRowHeading = (cellData) => {
-		const data = Object.keys(cellData);
-		const itemToReturn = data.map((item) => <th>{item}</th>);
-		return itemToReturn;
-	};
+	if (error) return <p>ERROR</p>
 
 	return (
-		<Container>
-			<Table responsive hover>
-				<thead>
-					<tr>{renderRowHeading(data.getAllReservations[0])}</tr>
-				</thead>
-				<tbody>
-					{data.getAllReservations.map((data, index) => (
-						<tr>{renderRowCell(data)}</tr>
-					))}
-				</tbody>
-			</Table>
-		</Container>
+    <DataTable tableData={data.getAllReservations} />
 	);
 };
 
